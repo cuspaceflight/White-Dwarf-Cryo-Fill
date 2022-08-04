12a8ac3187e7bb2d90e547053c59c0e4a683a20e
Daniel Gibbons <61093535+dug20@users.noreply.github.com>
2022-08-04 12:39:44 +0100

    Update fill.py

--- fill.py
+++ fill.py
@@ -27,17 +27,17 @@ k_n2o = 103.0e-3                                                                
 cp_n2o_l = PropsSI('CPMASS','T', T_bulk_n2o,'Q', 0, 'NITROUSOXIDE')                                                     # J/kg/K
 
 # Order of magnitude of internal conduction for boiling
-Q_internal = k_n2o * (np.pi * r_o**2) * (T_bulk_n2o - T_boil_n2o)/L      # Assume axial conduction from bottom of tank to top
+Q_internal = k_n2o * (np.pi * r_o**2) * (T_bulk_n2o - T_boil_n2o)/L      # Fourier's law. Assume axial conduction from bottom of tank to top
 mdot_boiloff = Q_internal / h_boil_n2o
 
 # Order of magnitude estimate for heat transfer through walls
 r_i = r_o - t
-R_walls = np.log(r_o / r_i) / (2 * np.pi * k_tank * L)
+R_walls = np.log(r_o / r_i) / (2 * np.pi * k_tank * L)          # Thermal resistance for radial conduction
 Q_walls = (T_air - T_bulk_n2o) / R_walls
 T_bulk_eqlm = T_air - Q_internal * R_walls
 
-dT_dt = (Q_internal - Q_walls) / (m_desired_n2o * cp_n2o_l)    # Q = m cp dT/dt, 
-t_to_chill = (T_bulk_n2o - T_desired_n2o) / dT_dt
+dT_dt = (Q_internal - Q_walls) / (m_desired_n2o * cp_n2o_l)     # Q = m cp dT/dt, 
+t_to_chill = (T_bulk_n2o - T_desired_n2o) / dT_dt               # Time to chill nitrous only (ignoring tank thermal mass)
 
 # Results
 print(f"""
