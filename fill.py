057d8cb5dd91bc241d37a310e9bd84a11e697fed
Daniel Gibbons <61093535+dug20@users.noreply.github.com>
2022-08-05 12:43:14 +0100

    Swap to valve-fixed boil off rate

--- fill.py
+++ fill.py
@@ -1,11 +1,15 @@
 import numpy as np
+import scipy.optimize
 from CoolProp.CoolProp import PropsSI
+
+# Valve properties
+D_fill = 8e-3
+D_vent = 1.5e-3
 
 # Tank dimensions
 L = 100e-2                  # m
 r_o = 15e-2                 # m
 t = 14e-3                   # m
-
 
 # Tank properties
 k_tank = 236                # W/m/K
@@ -15,34 +19,70 @@ k_tank = 236                # W/m/K
 T_air = 273.15 + 30         # K
 p_air = 1.0125e5            # Pa
 
-# Flow system and nitrous properties.
-T_bulk_n2o = T_air          # K
-m_desired_n2o = 5           # Desired mass of N2O in filled tank, kg
-T_desired_n2o = 273.15 - 10 # Desired nitrous temperature
-#mdot_fill_n2o = 0.1        # kg/s
+# Supply tank - saturated
+T_supply = T_air
+p_v_supply = PropsSI('P','T', T_supply,'Q', 0, 'NITROUSOXIDE')   
+p_supply = p_v_supply
 
-T_boil_n2o = PropsSI('T','P', p_air,'Q', 0, 'NITROUSOXIDE')                                                             # K
-h_boil_n2o = PropsSI('HMASS','P', p_air,'Q', 1, 'NITROUSOXIDE') - PropsSI('HMASS','P', p_air,'Q', 0, 'NITROUSOXIDE')    # J/kg
-k_n2o = 103.0e-3                                                                                                        # W/m/K, http://edge.rit.edu/edge/P07106/public/Nox.pdf
-cp_n2o_l = PropsSI('CPMASS','T', T_bulk_n2o,'Q', 0, 'NITROUSOXIDE')                                                     # J/kg/K
+def get_mdots(p_run):
+    # Use Dyer to predict fill mass flow rate
+    def mdot_dyer(p2):
+        Cd = 0.7
+        A = np.pi * (D_fill/2)**2
+        k = np.sqrt((p_supply - p2) / (p_v_supply - p2))
+        s1 = PropsSI('S','T', T_supply,'Q', 0, 'NITROUSOXIDE')   
+        h1 = PropsSI('HMASS','T', T_supply,'Q', 0, 'NITROUSOXIDE')   
+        rho_1 = PropsSI('DMASS','T', T_supply,'Q', 0, 'NITROUSOXIDE')
+        rho_2 = PropsSI('DMASS','S', s1,'P', p2, 'NITROUSOXIDE')  
+        h2 = PropsSI('HMASS','S', s1,'P', p2, 'NITROUSOXIDE')  
+        mdot_hem = Cd * A * rho_2 * np.sqrt(2 * (h1 - h2))
+        mdot_spi = Cd * A * np.sqrt(2 * rho_1 * (p_supply - p2))
+        return A * (k / (1 + k) *  mdot_spi + 1 / (1 + k) * mdot_hem)
 
-# Order of magnitude of internal conduction for boiling
-Q_internal = k_n2o * (np.pi * r_o**2) * (T_bulk_n2o - T_boil_n2o)/L      # Fourier's law. Assume axial conduction from bottom of tank to top
-mdot_boiloff = Q_internal / h_boil_n2o
+    mdot_crit = max(mdot_dyer(np.linspace(0, p_supply, 100)))
+    mdot_fill = min(mdot_dyer(p_run), mdot_crit)
+
+    # Use SPI to predict vent mass flow rate
+    Cd = 0.7
+    A = np.pi * (D_vent/2)**2 
+    rho_1 = PropsSI('DMASS','P', p_run,'Q', 1, 'NITROUSOXIDE')
+    mdot_vent = Cd * A * np.sqrt(2 * rho_1 * (p_run - p_air))
+
+    return mdot_vent, mdot_fill
+
+def mdot_error(p_run):
+    mdot_vent, mdot_fill = get_mdots(p_run)
+    return mdot_vent - mdot_fill
+
+p_run = scipy.optimize.root_scalar(mdot_error, bracket = [p_air + 100, p_supply - 100]).root
+mdot_vent, mdot_fill = get_mdots(p_run)
+
+# Calculate heat loss through venting
+dh_boiloff = PropsSI('HMASS','P', p_run,'Q', 1, 'NITROUSOXIDE') - PropsSI('HMASS','P', p_run,'Q', 0, 'NITROUSOXIDE')   
+Q_vent = mdot_vent * dh_boiloff
 
 # Order of magnitude estimate for heat transfer through walls
+T_run = PropsSI('T','P', p_run,'Q', 0, 'NITROUSOXIDE')   
 r_i = r_o - t
 R_walls = np.log(r_o / r_i) / (2 * np.pi * k_tank * L)          # Thermal resistance for radial conduction
-Q_walls = (T_air - T_bulk_n2o) / R_walls
-T_bulk_eqlm = T_air - Q_internal * R_walls
+Q_walls = (T_air - T_run) / R_walls
+T_bulk_eqlm = T_air - Q_vent * R_walls
 
-dT_dt = (Q_internal - Q_walls) / (m_desired_n2o * cp_n2o_l)     # Q = m cp dT/dt, 
-t_to_chill = (T_bulk_n2o - T_desired_n2o) / dT_dt               # Time to chill nitrous only (ignoring tank thermal mass)
+# Time to chill
+m_desired_n2o = 5           # Desired mass of N2O in filled tank, kg
+T_desired_n2o = 273.15 - 10 # Desired nitrous temperature
+
+cp_n2o_l = PropsSI('CPMASS','P', p_run,'Q', 0, 'NITROUSOXIDE')   
+dT_dt = (Q_vent - Q_walls) / (m_desired_n2o * cp_n2o_l)     # Q = m cp dT/dt, 
+t_to_chill = (T_run - T_desired_n2o) / dT_dt               # Time to chill nitrous only (ignoring tank thermal mass)
 
 # Results
 print(f"""
-Boil-off rate = {mdot_boiloff} kg/s
+Boil-off rate = {mdot_vent} kg/s
+Heat loss by vent = {Q_vent} W
+Heat in by conduction = {Q_walls} W
+Run pressure = {p_run/1e5} bar
 dT/dt = {dT_dt} K/s
-Time to chill = {t_to_chill/3600} hours
+Time to chill = {t_to_chill} s
 Equilibrium N2O temperature = {T_bulk_eqlm - 273.15} deg C
 """)
